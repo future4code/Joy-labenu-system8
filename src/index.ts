1852626f82333b79cf3e6ff4e781155c836fa35a
unknown <victorsantos732@gmail.com>
2022-05-16 21:19:48 -0300

    Criei as pastas iniciais do projeto

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { app } from "./app";
+
+
+app.post("/class", )
+
+app.get("/class/active", )
+
+app.put("/class/:id/module", )
+
+app.post("/student", )
+
+app.get("/student", )
+
+app.put("/student/:id/class", )
+
+app.post("/teacher", )
+
+app.get("/teacher", )
+
+app.post("/teacher/:id/class", )
